c6773d28acd36989f17cf7fad85d01200cc61254
aid7nn <aidan.molyneux82@gmail.com>
2026-02-28 15:40:33 +0000

    Clean Build
    Fresh FPS C++ Template

--- Source/TEST/TEST.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TEST.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TEST, "TEST" );
-
-DEFINE_LOG_CATEGORY(LogTEST)
